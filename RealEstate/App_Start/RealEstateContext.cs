0c7dc8c250fac40c169113675254625b720e73e7
Bobby Mundy <robert.l.mundy@gmail.com>
2016-02-26 17:56:09 -0500

    Initial Commit

---
+++ RealEstate/App_Start/RealEstateContext.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+using RealEstate.Properties;
+
+namespace RealEstate.App_Start
+{
+	public class RealEstateContext
+	{
+		public MongoDatabase Database;
+
+		public RealEstateContext()
+		{
+			var client = new MongoClient(Settings.Default.RealEstateConnectionString);
+			MongoServer server = client.GetServer();
+			Database = server.GetDatabase(Settings.Default.RealEstateDatabaseName);
+		}
+	}
+}
